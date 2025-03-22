d64285e561055b698676a7146fce65fe4b70bcab
TheLastCD <lambopuggy@gmail.com>
2025-03-22 11:47:10 +0000

    more local changes to fix git history

--- sharedobj/dynmain.c
+++
@@ -1,9 +0,0 @@
-#include <dlfcn.h>
-void hello ();
-
-
-void main(){
-	void* handle = dlopen("libhello.so",RTLD_NOW);
-	void(*helloFunc)(void) = dlsym(handle, "hello");
-	helloFunc();
-}
--- sharedobj/main.c
+++
@@ -1,6 +0,0 @@
-void hello ();
-
-
-void main(){
-	hello();
-}
